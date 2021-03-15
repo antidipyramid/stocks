487b1294a402bd78bf40aaa6fc968658cfd433bc
antidipyramid <mstj77@protonmail.com>
2021-03-15 14:51:30 -0400

    Added viewset for assets and senators

--- senate_stocks/stocks/api/urls.py
+++ senate_stocks/stocks/api/urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls import url
 from django.urls import re_path, path, include
-from .views import TradeListApiView, SearchApiViewSet
+from .views import TradeListApiView, SearchApiViewSet, SenatorViewSet, AssetViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('senators', SenatorViewSet, basename='senator')
+router.register('assets', AssetViewSet, basename='asset')
 
 urlpatterns = [
     path('', TradeListApiView.as_view()),
     path('search/<str:query>/', SearchApiViewSet.as_view({'get':'list'})),
 ]
+
+urlpatterns.extend(router.urls)
--- senate_stocks/stocks/api/views.py
+++ senate_stocks/stocks/api/views.py
@@ -1,13 +1,15 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
 from rest_framework import permissions
 from stocks.models import Trade, Senator, Asset
-from .serializers import TradeSerializer, SearchSerializer
+from .serializers import TradeSerializer, SearchSerializer, SenatorSerializer, AssetSerializer
 from django.db.models import Q
 
 from collections import namedtuple
 from datetime import date, timedelta
+
 
 class TradeListApiView(APIView):
 
@@ -20,10 +22,52 @@ class TradeListApiView(APIView):
         serializer = TradeSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+class AssetViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing asset models.
+    """
+    queryset = Asset.objects.all()
+    serializer_class = AssetSerializer
+
+    def list(self, request):
+        serializer = self.serializer_class(self.queryset,many=True)
+        return Response(serializer.data)
+
+    def retrive(self, request, pk=None):
+        asset = get_object_or_404(self.queryset,pk=pk)
+        serializer = AssetSerializer(asset)
+        return Response(serializer.data)
+
+"""
+class SenatorListApiView(APIView):
+
+    permission_classes = [permissions.IsAuthenticated]
+    def get(self, request, *args, **kwargs):
+        queryset = Senator.objects.all()
+        serializer = SenatorSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+"""
+
+class SenatorViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing the senator models.
+    """
+    queryset = Senator.objects.all()
+    serializer_class = SenatorSerializer
+
+    def list(self, request):
+        serializer = self.serializer_class(self.queryset,many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        senator = get_object_or_404(self.queryset,pk=pk)
+        serializer = SenatorSerializer(senator)
+        return Response(serializer.data)
+
 class SearchApiViewSet(viewsets.ViewSet):
     """
     This viewset searches for the provided query among the senators and companies
-    and puts the results in a neat little object for the serializer to parse 
+    and puts the results in a neat little object for the serializer to parse
     """
     permission_classes = [permissions.IsAuthenticated]
     def list(self, request, query):
