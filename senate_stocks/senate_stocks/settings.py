2892f71c9bc7f9aaf2ada0a51979315661574047
antidipyramid <mstj77@protonmail.com>
2021-10-28 16:39:37 -0400

    changed settings

--- senate_stocks/senate_stocks/settings.py
+++ senate_stocks/senate_stocks/settings.py
@@ -92,9 +92,7 @@ except ImportError:
     DEBUG = False
 
     DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        }
+        'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
     }
 
     db_from_env = dj_database_url.config(conn_max_age=500)
